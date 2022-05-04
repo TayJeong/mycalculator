1d2becee2f39f940c98cdb7dd4ef36239a2f5a16
TayJeong <100740917+TayJeong@users.noreply.github.com>
2022-05-04 09:27:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import setuptools
 
 setuptools.setup (
    include_package_data = True,
-  name = 'mycalculator01',
+  name = 'mycalculator0001',
   version='1.0.0',
   description = 'oss-dev my calculator',
   author='TayJeong',
   author_email='2052862@donga.ac.kr',
   url = 'https://github.com/TayJeong/mycalculator/',
+  download_url = "https://github.com/TayJeong/mycalculator/releases/tag/v.1.0.0",
   install_requires=['pytest'],
   long_description = 'oss-dev calculator python module',
   long_description_content_type = 'text/markdown',
